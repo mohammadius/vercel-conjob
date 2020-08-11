cecb3819cde57355599ad89798be6b6d3addc535
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2020-08-12 02:08:23 +0430

    removed authorization

--- pages/api/discordWebhooks.ts
+++ pages/api/discordWebhooks.ts
@@ -3,9 +3,6 @@ import { env } from "process";
 import YoutubeRssFeed from "./_youtubeRssFeed";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const auth = req.headers.authorization;
-	if (auth !== env.authPass) res.status(401).end();
-
 	await YoutubeRssFeed("https://www.youtube.com/feeds/videos.xml?channel_id=UC-lHJZR3Gqxm24_Vd_AJ5Yw", env.pewdiepieWebhookUrl);
 	await YoutubeRssFeed("https://www.youtube.com/feeds/videos.xml?channel_id=UCBtvdchuXXrYoovpc5fPlSQ", env.bikinibodhiWebhookUrl);
 

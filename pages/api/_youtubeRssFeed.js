4037ef4bdf3894946396ec25529a78f900f2a46a
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2020-08-12 02:25:36 +0430

    added date-diff

--- pages/api/_youtubeRssFeed.js
+++ pages/api/_youtubeRssFeed.js
@@ -1,16 +1,19 @@
 import log from "./_log";
 import axios from "axios";
+import DateDiff from "date-diff";
 import Parser from "rss-parser";
 let parser = new Parser();
 
-export default async (rssUrl: string, webhookUrl: string) => {
+export default async (rssUrl, webhookUrl) => {
 	const { items, title } = await parser.parseURL(rssUrl);
 
 	let newItems = items.filter((item) => {
-		let pubDate = new Date(item.pubDate);
-		let nowDate = new Date();
-		var diffMins = Math.round((((nowDate.getDate() - pubDate.getDate()) % 86400000) % 3600000) / 60000);
-		return diffMins < 21;
+		const pubDate = new Date(item.pubDate);
+		const nowDate = new Date();
+
+		const diff = new DateDiff(nowDate, pubDate);
+
+		return diff.minutes() < 21;
 	});
 
 	await log(title, `recieved ${items.length} rss feed total\n${newItems.length > 0 ? newItems.length : "none"} of them are new`);

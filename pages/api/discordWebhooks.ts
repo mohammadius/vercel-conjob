0d5c5ee2410a3c9c245a82bf0b2f7ccc34fb2427
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2020-08-13 20:05:33 +0430

    added Braille Skateboarding

--- pages/api/discordWebhooks.ts
+++ pages/api/discordWebhooks.ts
@@ -6,6 +6,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	await YoutubeRssFeed("https://www.youtube.com/feeds/videos.xml?channel_id=UC-lHJZR3Gqxm24_Vd_AJ5Yw", env.pewdiepieWebhookUrl);
 	await YoutubeRssFeed("https://www.youtube.com/feeds/videos.xml?channel_id=UCBtvdchuXXrYoovpc5fPlSQ", env.bikinibodhiWebhookUrl);
 	await YoutubeRssFeed("https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw", env.linustechtipsWebhookUrl);
+	await YoutubeRssFeed(
+		"https://www.youtube.com/feeds/videos.xml?channel_id=UC9PgszLOAWhQC6orYejcJlw",
+		env.brailleskateboardingWebhookUrl
+	);
 
 	res.status(204).end();
 };
